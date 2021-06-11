b68ac0950abfcfc1b1b41c18af3c14604231997c
Fatilda IV <30930670+FatildaIV@users.noreply.github.com>
2021-06-11 12:43:12 +0200

    Initial commit

---
+++ CS005NasobeniCisel/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS005NasobeniCisel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void vynasobitButton_Click(object sender, EventArgs e)
+        {
+            vysledekLabel.Text =
+                string.Format(
+                    "={0:0.000}",
+                    double.Parse(cislo1TextBox.Text) * 
+                    double.Parse(cislo2TextBox.Text) * 
+                    double.Parse(cislo3TextBox.Text)
+                );
+        }
+    }
+}
